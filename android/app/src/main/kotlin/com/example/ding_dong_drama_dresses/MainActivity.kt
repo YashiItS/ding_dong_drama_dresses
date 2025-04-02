e804fc88bc07439a8f0305ed4da25ec1259fa447
imkaransharmaaa <ks228182@gmail.com>
2025-04-02 10:30:34 +0530

    first push

---
+++ android/app/src/main/kotlin/com/example/ding_dong_drama_dresses/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ding_dong_drama_dresses
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/routes/routes_name.dart
@@ -0,0 +1,7 @@
+class RoutesName {
+  static const String splashScreen = 'splash_screen';
+  static const String homeScreen = 'splash_screen';
+  static const String loginScreen = 'splash_screen';
+  static const String otpScreen = 'splash_screen';
+  static const String searchScreen = 'splash_screen';
+}
